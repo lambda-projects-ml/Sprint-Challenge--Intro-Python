8925c4e71f478bc3ed37d9aba01542b4c14c1237
Mike Landers <landers.mike@gmail.com>
2019-04-26 08:16:50 -0700

    Completed oop1 and oop2

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,31 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+
+class Vehicle():  # base class
+    pass
+
+
+class FlightVehicle(Vehicle):  # base class
+    pass
+
+
+class Starship(FlightVehicle):  # base class
+    pass
+
+
+class GroundVehicle(Vehicle):  # base class is Vehicle
+    pass
+
+
+class Airplane(FlightVehicle):  # base class is FlightVehicle
+    pass
+
+
+class Car(GroundVehicle):  # base class is GroundVehicle
+    pass
+
+
+class Motorcycle(GroundVehicle):  # base class is GroundVehicle
+    pass
